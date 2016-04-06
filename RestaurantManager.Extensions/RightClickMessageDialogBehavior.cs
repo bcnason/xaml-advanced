95d708b95ca445a127651586fe7aab697d353f70
bcnason <bcnason@yahoo.com>
2016-04-06 13:26:27 -0400

    Implemented Right Click Behavior and Color Converter
    Support messaging for right click on main page.  Add color converter to
    highlight expedited orders on expedite page.

---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+
+    {
+        public string Message { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += Page_RightTapped;
+            }
+        }
+
+        public void Detach()
+        {
+            if(this.AssociatedObject !=null && this.AssociatedObject is Page)
+            {
+                this.AssociatedObject = null;
+                (this.AssociatedObject as Page).RightTapped -= Page_RightTapped;
+            }
+        }
+
+        private async void Page_RightTapped(object sender, RightTappedRoutedEventArgs e)
+       {
+            await new MessageDialog(this.Message).ShowAsync();
+        }
+    }
+
+}
